6200205181566461f4117cf471fa3a208b8ca90a
guneves <wgrrodrigues6@gmail.com>
2025-11-10 23:04:20 -0300

    comments

--- src/escalonadores/fifo.py
+++ src/escalonadores/fifo.py
@@ -1,33 +1,9 @@
-"""
-Exemplo:
-
 from typing import Optional
 
 from .base import EscalonadorBase
 from ..processo import Processo
 
 class EscalonadorFIFO(EscalonadorBase):
-    
-    def adicionar_processo(self, processo: Processo, tempo_atual: int):
-        self.fila_prontos.append(processo)
-
-    def proximo_processo(self) -> Optional[Processo]:
-        if self.fila_prontos:
-            return self.fila_prontos.pop(0)
-        return None
-"""
-from typing import Optional
-
-from .base import EscalonadorBase
-from ..processo import Processo
-
-class EscalonadorFIFO(EscalonadorBase):
-    """
-    Implementa o escalonador First-Come, First-Served (FIFO/FCFS). z 
-    É um algoritmo não preemptivo.
-    não implementa quantum ou fatias de tempo, não verifica preempção.
-    """
-    
     def adicionar_processo(self, processo: Processo, tempo_atual: int):
         """
         Adiciona um processo ao final da fila de prontos.
